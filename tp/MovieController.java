2765aa5925e15531595a5d8b7396d61d45d9f777
PaulaNogueira <paulanogueiraccarvalho@gmail.com>
2025-03-07 21:12:23 -0300

    comecando o controller

--- tp/MovieController.java
+++ tp/MovieController.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class MovieController {
     
+    public boolean insert(Scanner scanner) {
+  
+    }
+
+    public boolean update(Scanner scanner) {
+
+    }
+
+    public boolean delete(Scanner scanner) {
+ 
+    }
+
+    public void get(Scanner scanner) {
+
+    }
 }
